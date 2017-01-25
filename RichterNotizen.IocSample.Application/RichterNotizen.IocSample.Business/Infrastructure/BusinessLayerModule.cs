ca48774f7d36f46c9814044f3ec2391a35574b8c
Lars Richter <lars.richter@neveling.net>
2017-01-25 23:06:26 +0100

    Added modules for data and business layer.

---
+++ RichterNotizen.IocSample.Application/RichterNotizen.IocSample.Business/Infrastructure/BusinessLayerModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace RichterNotizen.IocSample.Business.Infrastructure
+{
+    public class BusinessLayerModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
---
+++ RichterNotizen.IocSample.Application/RichterNotizen.IocSample.Data/Infrastructure/DataLayerModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace RichterNotizen.IocSample.Data.Infrastructure
+{
+    public class DataLayerModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
