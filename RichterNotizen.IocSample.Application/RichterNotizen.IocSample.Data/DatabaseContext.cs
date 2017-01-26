3bc9dd9d3e296816faeff7bf94c3996a1c66a004
Lars Richter <lars.richter@neveling.net>
2017-01-26 21:44:15 +0100

    Fix for EntityFramecork.SqlServer reference issue.

--- RichterNotizen.IocSample.Application/RichterNotizen.IocSample.Data/DatabaseContext.cs
+++ RichterNotizen.IocSample.Application/RichterNotizen.IocSample.Data/DatabaseContext.cs
@@ -6,8 +6,16 @@ namespace RichterNotizen.IocSample.Data
     {
         public DatabaseContext() : base("name=IocSample")
         {
-
         }
+
         public DbSet<Product> Products { get; set; }
+
+        /*
+         * This method is never used. It exists, because of a reference-issue with the EntityFramecork.SqlServer.dll
+         */
+        public void EntityFrameworkSqlServerBinaryFix()
+        {
+            var instance = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+        }
     }
 }
