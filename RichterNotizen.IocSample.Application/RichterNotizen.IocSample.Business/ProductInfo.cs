f89353a89f739e0ec0b0e822d4ba137827e6e0a5
Lars Richter <lars.richter@neveling.net>
2017-01-26 12:45:32 +0100

    Added a service to the business layer.

---
+++ RichterNotizen.IocSample.Application/RichterNotizen.IocSample.Business/ProductInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace RichterNotizen.IocSample.Business
+{
+    public class ProductInfo
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ RichterNotizen.IocSample.Application/RichterNotizen.IocSample.Business/ProductService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace RichterNotizen.IocSample.Business
+{
+    public class ProductService : IProductService
+    {
+        public List<ProductInfo> GetTopSeller()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
