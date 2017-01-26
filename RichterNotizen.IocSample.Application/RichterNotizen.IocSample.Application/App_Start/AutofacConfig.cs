5887a9dc686f30d8c16c043e228d11914605e2ca
Lars Richter <lars.richter@neveling.net>
2017-01-26 13:40:18 +0100

    Added configuration for Autofac in application layer.

---
+++ RichterNotizen.IocSample.Application/RichterNotizen.IocSample.Application/App_Start/AutofacConfig.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using RichterNotizen.IocSample.Business.Infrastructure;
+using System.Web.Mvc;
+
+namespace RichterNotizen.IocSample.Application
+{
+    public class AutofacConfig
+    {
+        public static void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            builder.RegisterModule<BusinessLayerModule>();
+
+            var container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
