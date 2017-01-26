ab86a68b5c73479cdba4774908b6b8d1a96f5676
Lars Richter <lars.richter@neveling.net>
2017-01-26 13:56:59 +0100

    Created a top seller functionality in application layer.

---
+++ RichterNotizen.IocSample.Application/RichterNotizen.IocSample.Application/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using RichterNotizen.IocSample.Business;
+using System.Web.Mvc;
+
+namespace RichterNotizen.IocSample.Application.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // GET: Products
+        public ActionResult TopSeller()
+        {
+            var topSeller = _productService.GetTopSeller();
+            return View(topSeller);
+        }
+    }
+}
