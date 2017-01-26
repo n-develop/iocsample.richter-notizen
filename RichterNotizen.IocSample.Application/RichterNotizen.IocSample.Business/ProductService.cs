9412f16448e87347e39f1ebbd5ddaa650c7ca76a
Lars Richter <lars.richter@neveling.net>
2017-01-26 13:56:40 +0100

    Added functionality to product service.

--- RichterNotizen.IocSample.Application/RichterNotizen.IocSample.Business/ProductService.cs
+++ RichterNotizen.IocSample.Application/RichterNotizen.IocSample.Business/ProductService.cs
@@ -1,13 +1,23 @@
-using System;
+using RichterNotizen.IocSample.Data;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RichterNotizen.IocSample.Business
 {
     public class ProductService : IProductService
     {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
         public List<ProductInfo> GetTopSeller()
         {
-            throw new NotImplementedException();
+            var products = _productRepository.GetAllProducts();
+            var productInfos = products.Select(product => new ProductInfo { Name = product.Sku + " - " + product.Name, Price = product.Price }).ToList();
+            return productInfos;
         }
     }
 }
